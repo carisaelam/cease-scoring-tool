4eda18c025ae3f67bc52a1881b7beaf0528b4bf2
carisaelam <carisaelam@gmail.com>
2025-10-07 19:25:01 -0400

    Move questions to lib file and populate

---
+++ src/lib/questions.ts
@@ -0,0 +1,50 @@
+export const QUESTIONS = [
+  {
+    id: 'q1',
+    text: 'Have you ever had sex or performed sex acts in exchange for things of value (for example: money, housing, drugs, food, etc.)?',
+  },
+  {
+    id: 'q1a',
+    text: `Were you a minor or under the age of 18`,
+  },
+  {
+    id: 'q1b',
+    text: 'Was there ever a time when someone pressured, forced, or manipulated you to engage in sex or sex acts in exchange for something of value?',
+  },
+  {
+    id: 'q2',
+    text: 'If you answered yes to 1(a), were any of your arrest(s) or conviction(s) connected to or occur during that situation, or did any of your arrest(s) or conviction(s) occur during it?',
+  },
+  {
+    id: 'q3',
+    text: 'Has anyone ever taken a portion or all of the money you earned from work or services?',
+  },
+  {
+    id: 'q4',
+    text: 'Has anyone ever forced you to make a certain amount of money on a daily or weekly basis?',
+  },
+  {
+    id: 'q5',
+    text: 'Has anyone ever pressured, forced, or manipulated you into doing different work or services than you originally agreed to?',
+  },
+  {
+    id: 'q6',
+    text: 'Has anyone ever forced you to get or use false documents',
+  },
+  {
+    id: 'q7',
+    text: 'Has anyone you were working for or associated with ever taken and kept your identification documents (i.e., passport, birth certificate, visa, driver’s license/ID)?',
+  },
+  {
+    id: 'q8',
+    text: 'Was the situation where someone pressured, forced, or manipulated you into working (including exchanging sex acts for things of value) connected to any arrest(s) or conviction(s) you have received?',
+  },
+  {
+    id: 'q9',
+    text: 'Were you ever arrested or convicted for something that happened while you were being directed or influenced by someone who was pressuring, forcing, or manipulating you?',
+  },
+  {
+    id: 'q10',
+    text: 'If initial screening indicates that you may be eligible for remedies under the Survivors First Act, would you like to be contacted to schedule an interview?',
+  },
+] as const
--- src/pages/FormPage.vue
+++ src/pages/FormPage.vue
@@ -9,288 +9,75 @@
         {{ errorMsg }}
       </div>
 
+      <!-- Form -->
       <form>
-        <!-- First Name -->
-        <div class="mb-3">
-          <label for="firstName" class="form-label">Legal First Name</label>
-          <input
-            v-model="store.demographics.firstName"
-            type="text"
-            id="firstName"
-            class="form-control"
-          />
+        <!-- DEMOGRAPHICS -->
+        <div class="demographics__form__container">
+          <!-- First Name -->
+          <div class="mb-3">
+            <label for="firstName" class="form-label">Legal First Name</label>
+            <input
+              v-model="store.demographics.firstName"
+              type="text"
+              id="firstName"
+              class="form-control"
+            />
+          </div>
+          <!-- Last Name -->
+          <div class="mb-3">
+            <label for="lastName" class="form-label">Legal Last Name</label>
+            <input
+              v-model="store.demographics.lastName"
+              type="text"
+              id="lastName"
+              class="form-control"
+            />
+          </div>
+          <!-- Date of Birth -->
+          <div class="mb-3">
+            <label for="dob" class="form-label">Date of Birth</label>
+            <input v-model="store.demographics.dob" type="date" id="dob" class="form-control" />
+          </div>
+          <!-- GDC Inmate Number -->
+          <div class="mb-3">
+            <label for="gdcNumber" class="form-label">GDC Inmate Number</label>
+            <input
+              v-model="store.demographics.gdcNumber"
+              type="text"
+              id="gdcNumber"
+              class="form-control"
+              inputmode="numeric"
+              autocomplete="off"
+            />
+          </div>
         </div>
 
-        <!-- Last Name -->
-        <div class="mb-3">
-          <label for="lastName" class="form-label">Legal Last Name</label>
-          <input
-            v-model="store.demographics.lastName"
-            type="text"
-            id="lastName"
-            class="form-control"
-          />
-        </div>
-
-        <!-- Date of Birth -->
-        <div class="mb-3">
-          <label for="dob" class="form-label">Date of Birth</label>
-          <input v-model="store.demographics.dob" type="date" id="dob" class="form-control" />
-        </div>
-
-        <!-- GDC Inmate Number -->
-        <div class="mb-3">
-          <label for="gdcNumber" class="form-label">GDC Inmate Number</label>
-          <input
-            v-model="store.demographics.gdcNumber"
-            type="text"
-            id="gdcNumber"
-            class="form-control"
-            inputmode="numeric"
-            autocomplete="off"
-          />
-        </div>
-
-        <!-- Questions -->
-        <div class="question__container">
-          <!-- Question 1 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 1: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q1"
-                id="q1-yes"
-              />
-              <label class="form-check-label" for="q1-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q1"
-                id="q1-no"
-              />
-              <label class="form-check-label" for="q1-no">No</label>
-            </div>
-          </div>
-          <!-- Question 2 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 2: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q2"
-                id="q2-yes"
-              />
-              <label class="form-check-label" for="q2-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q2"
-                id="q2-no"
-              />
-              <label class="form-check-label" for="q2-no">No</label>
-            </div>
-          </div>
-          <!-- Question 3 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 3: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q3"
-                id="q3-yes"
-              />
-              <label class="form-check-label" for="q3-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q3"
-                id="q3-no"
-              />
-              <label class="form-check-label" for="q3-no">No</label>
-            </div>
-          </div>
-          <!-- Question 4 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 4: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q4"
-                id="q4-yes"
-              />
-              <label class="form-check-label" for="q4-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q4"
-                id="q4-no"
-              />
-              <label class="form-check-label" for="q4-no">No</label>
-            </div>
-          </div>
-          <!-- Question 5 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 5: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q5"
-                id="q5-yes"
-              />
-              <label class="form-check-label" for="q5-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q5"
-                id="q5-no"
-              />
-              <label class="form-check-label" for="q5-no">No</label>
-            </div>
-          </div>
-          <!-- Question 6 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 6: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q6"
-                id="q6-yes"
-              />
-              <label class="form-check-label" for="q6-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q6"
-                id="q6-no"
-              />
-              <label class="form-check-label" for="q6-no">No</label>
-            </div>
-          </div>
-          <!-- Question 7 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 7: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q7"
-                id="q7-yes"
-              />
-              <label class="form-check-label" for="q7-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q7"
-                id="q7-no"
-              />
-              <label class="form-check-label" for="q7-no">No</label>
-            </div>
-          </div>
-          <!-- Question 8 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 8: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q8"
-                id="q8-yes"
-              />
-              <label class="form-check-label" for="q8-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q8"
-                id="q8-no"
-              />
-              <label class="form-check-label" for="q8-no">No</label>
-            </div>
-          </div>
-          <!-- Question 9 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 9: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q9"
-                id="q9-yes"
-              />
-              <label class="form-check-label" for="q9-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q9"
-                id="q9-no"
-              />
-              <label class="form-check-label" for="q9-no">No</label>
-            </div>
-          </div>
-          <!-- Question 10 -->
-          <div class="mb-3">
-            <label class="form-label d-block">Question 10: Example text goes here?</label>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="true"
-                v-model="store.answers.q10"
-                id="q10-yes"
-              />
-              <label class="form-check-label" for="q10-yes">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                :value="false"
-                v-model="store.answers.q10"
-                id="q10-no"
-              />
-              <label class="form-check-label" for="q10-no">No</label>
+        <!-- QUESTIONS -->
+        <div class="questions__form__container">
+          <!-- Question loop -->
+          <div v-for="question in QUESTIONS" v-bind:key="question.id">
+            <div class="mb-3">
+              <label class="form-label d-block">{{ question.id }}: {{ question.text }}</label>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  :value="true"
+                  v-model="store.answers[question.id]"
+                  :id="question.id + '-yes'"
+                />
+                <label class="form-check-label" :for="question.id + '-yes'">Yes</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  :id="question.id + '-no'"
+                  :value="false"
+                  v-model="store.answers[question.id]"
+                />
+                <label class="form-check-label" :for="question.id + '-no'">No</label>
+              </div>
             </div>
           </div>
         </div>
@@ -310,6 +97,7 @@
 <script setup lang="ts">
 import { useSubmissionStore } from '@/stores/submission'
 import { useRouter } from 'vue-router'
+import { QUESTIONS } from '../lib/questions.ts'
 
 import { ref } from 'vue'
 const router = useRouter()
